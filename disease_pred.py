83182bd76c6d5fc7344515bacd5cf07534c5d3dd
manojkumar16122 <115546459+manojkumar16122@users.noreply.github.com>
2023-02-18 16:21:44 +0530

    Update disease_pred.py

--- disease_pred.py
+++ disease_pred.py
@@ -284,4 +284,4 @@ if (selected == 'About us'):
 if (selected == 'Contact'):
     st.title('FURTHER CONTACT DETAILS')
     st.text('For further details contact us at mkinfotechology2022@gmail.com')
-    st.text('Developed by Manojkumar and Thanapraneshkumar')
+    st.text('Developed by Manojkumar')

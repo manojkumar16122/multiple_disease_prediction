907c1968ee520141f1d67e60eae6dfe7240e40d8
Manojkumar V <115546459+manojkumar16122@users.noreply.github.com>
2023-11-08 10:19:57 +0530

    Update disease_pred.py

--- disease_pred.py
+++ disease_pred.py
@@ -245,41 +245,55 @@ if (selected == "Parkinsons Prediction"):
     st.success(parkinsons_diagnosis)
     
 if (selected == 'Autism Prediction'):
-    
-    st.title('Autism Disease Prediction Using ML')
-    
-    
-    A1_Score = st.text_input('Whether the child look at you when you call his/her name?(Yes-1/No-0)')
-    A2_Score = st.text_input('How easy it is for you to get eye contact with the child?(Yes-1/No-0)')
-    A3_Score = st.text_input('Does your child point to indicate that he/she wants something?(Yes-1/No-0)')
-    A4_Score = st.text_input('Does your child point to share interest with you?(Yes-1/No-0)')
-    A5_Score = st.text_input('Does your child pretend?(Yes-1/No-0)')
-    A6_Score = st.text_input('Does your child follow when you are looking?(Yes-1/No-0)')
-    A7_Score = st.text_input('If you or someone else in the family is visibly upset,does your child show signs of warning to comfort them?(Yes-1/No-0)')
-    A8_Score = st.text_input('Does your child talk back when you talk?(Yes-1/No-0)')
-    A9_Score = st.text_input('Does your child use simple gestures?(Yes-1/No-0)')
-    A10_Score = st.text_input('Does your child stare at nothing with no apparent purpose?(Yes-1/No-0)')
-    age = st.text_input('Age')
-    jundice=st.text_input('Whether the child was born with jaundice?(Yes-1/No-0)')
-    
-    
-    autism_diagnosis = ''
-    
-    if st.button("AUTISM TEST RESULT"):
-        autism_prediction = autism_model.predict([[A1_Score,A2_Score,A3_Score,A4_Score,A5_Score,A6_Score,A7_Score,A8_Score,A9_Score,A10_Score,age,jundice]])
-        
-        if (autism_prediction[0] == 1):
-            autism_diagnosis = "The Person has the risk of Autism"
-        else:
-            autism_diagnosis = "The Person does not have Autism Disease"
-            st.success( autism_diagnosis)
-            "For further consultant kindly use:"
-            "1. ABA Therapy Centre for AUTISM link:" "http://www.youcanautism.com/"
-            "2. Third Eye - A learning center for Autism Link:" "http://www.thirdeyecenter.org/"
-            "3. Vatsalyam Centre For Autism Link:" "http://www.vatsalyam.in/"
-            "4. Sparks Vidyalaya Link:" "http://sparksautismschool.com/"
-         
-        
+
+st.title('Autism Disease Prediction Using ML')
+
+A1_Score = st.text_input('Whether the child looks at you when you call his/her name? (Yes-1/No-0)')
+A2_Score = st.text_input('How easy it is for you to get eye contact with the child? (Yes-1/No-0)')
+A3_Score = st.text_input('Does your child point to indicate that he/she wants something? (Yes-1/No-0)')
+A4_Score = st.text_input('Does your child point to share interest with you? (Yes-1/No-0)')
+A5_Score = st.text_input('Does your child pretend? (Yes-1/No-0)')
+A6_Score = st.text_input('Does your child follow when you are looking? (Yes-1/No-0)')
+A7_Score = st.text_input('If you or someone else in the family is visibly upset, does your child show signs of wanting to comfort them? (Yes-1/No-0)')
+A8_Score = st.text_input('Does your child talk back when you talk? (Yes-1/No-0)')
+A9_Score = st.text_input('Does your child use simple gestures? (Yes-1/No-0)')
+A10_Score = st.text_input('Does your child stare at nothing with no apparent purpose? (Yes-1/No-0)')
+age = st.text_input('Age')
+jundice = st.text_input('Whether the child was born with jaundice? (Yes-1/No-0)')
+
+autism_diagnosis = ''
+
+if st.button("AUTISM TEST RESULT"):
+    # Convert input values to integers
+    input_data = [
+        int(A1_Score),
+        int(A2_Score),
+        int(A3_Score),
+        int(A4_Score),
+        int(A5_Score),
+        int(A6_Score),
+        int(A7_Score),
+        int(A8_Score),
+        int(A9_Score),
+        int(A10_Score),
+        int(age),
+        int(jundice)
+    ]
+
+    # Predict autism using your model
+    autism_prediction = autism_model.predict([input_data])
+
+    if autism_prediction[0] == 1:
+        autism_diagnosis = "The person has a risk of Autism"
+    else:
+        autism_diagnosis = "The person does not have Autism Disease"
+        st.success(autism_diagnosis)
+        st.markdown("For further consultation, kindly use:")
+        st.markdown("1. ABA Therapy Centre for Autism link: [http://www.youcanautism.com/](http://www.youcanautism.com/)")
+        st.markdown("2. Third Eye - A learning center for Autism Link: [http://www.thirdeyecenter.org/](http://www.thirdeyecenter.org/)")
+        st.markdown("3. Vatsalyam Centre For Autism Link: [http://www.vatsalyam.in/](http://www.vatsalyam.in/)")
+        st.markdown("4. Sparks Vidyalaya Link: [http://sparksautismschool.com/](http://sparksautismschool.com/)")
+
 if (selected == 'About us'):
     st.title('About us')
     st.text('This application is designed to predict whether a person has the listed multiple disease or not.')
